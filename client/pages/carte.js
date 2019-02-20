cbcd500042c5d9c4f2946fe974415aabdebba2aa
HerboSan <antoine.herbosa@epitech.eu>
2019-02-20 10:32:09 +0100

    install fullpage section

--- client/pages/carte.js
+++ client/pages/carte.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import NavBar from '../components/nav-bar'
+
 
 export default class carte extends Component {
     render() {
--- client/pages/formulaire.js
+++ client/pages/formulaire.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import Fullpage, { FullPageSections, FullpageSection } from '@ap.cx/react-fullpage'
+import NavBar from '../components/nav-bar'
+
 
 export default class formulaire extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar/>
-      </div>
-    )
-  }
-}
+    render() {
+        return (
+            <div>
+            <NavBar/>
+            <Fullpage>
+            <FullPageSections>
+            <FullpageSection style={{
+                backgroundColor: 'lime',
+                padding: '1em',
+            }}>
+            1
+            </FullpageSection>
+            
+            <FullpageSection style={{
+                backgroundColor: 'coral',
+                padding: '1em',
+            }}>
+            2
+            </FullpageSection>
+            
+            <FullpageSection style={{
+                backgroundColor: 'firebrick',
+                padding: '1em',
+            }}>
+            3
+            </FullpageSection>
+            
+            </FullPageSections>
+            
+            </Fullpage>
+            </div>
+            )
+        }
+    }
+    
