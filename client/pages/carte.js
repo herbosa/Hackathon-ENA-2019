c9c07d920f9b7b08b1a4a95a5b026b64225ea162
HerboSan <antoine.herbosa@epitech.eu>
2019-02-20 16:05:49 +0100

    form

--- client/pages/carte.js
+++ client/pages/carte.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import NavBar from '../components/nav-bar'
 import {Helmet} from "react-helmet";
+import '../static/css/index.css'
+
 
 export default class carte extends Component {
     render() {
@@ -9,6 +11,7 @@ export default class carte extends Component {
     <Helmet>
         <title>Accueil</title>
         <meta name="description" content="Accueil" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"/>
 			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
 			<link rel="shortcut icon" href="../static/favicon.ico" type="image/x-icon"/>
 			<link rel="icon" href="../static/logo.png" type="image/png"/>
--- client/pages/index.js
+++ client/pages/index.js
@@ -10,7 +10,8 @@ const Home = () => (
     <Helmet>
         <title>Accueil</title>
         <meta name="description" content="Accueil" />
-			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"/>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
 			<link rel="shortcut icon" href="../static/favicon.ico" type="image/x-icon"/>
 			<link rel="icon" href="../static/logo.png" type="image/png"/>
       <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"/>
